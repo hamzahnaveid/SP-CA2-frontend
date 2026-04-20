fdc55a3af518af1d3b691f07f3670a1589175110
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-04-20 16:09:06 +0100

    Prefill Place Order form if values exist

--- src/app/customer/components/place-order/place-order.component.ts
+++ src/app/customer/components/place-order/place-order.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { CustomerService } from '../../services/customer.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-place-order',
@@ -12,14 +13,15 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class PlaceOrderComponent {
 
-  orderForm!: FormGroup
+  orderForm!: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private snackBar: MatSnackBar,
     private customerService: CustomerService,
     private router: Router,
-    public dialog: MatDialog 
+    public dialog: MatDialog,
+    public storage: StorageService
   ) {}
 
   ngOnInit() {
@@ -27,6 +29,16 @@ export class PlaceOrderComponent {
       address: ['', [Validators.required]],
       payment: ['', Validators.required]
     })
+
+    const user = this.storage.getUser();
+    if (user.shippingAddress != '' && user.paymentMethod != '') {
+      console.log(user);
+      const existingValues = {
+        address: user.shippingAddress,
+        payment: user.paymentMethod
+      }
+      this.orderForm.patchValue(existingValues);
+    }
   }
 
   placeOrder() {
